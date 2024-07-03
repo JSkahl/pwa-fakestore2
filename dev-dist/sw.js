135b680f6232c466ab1130715d8aa539479416b5
JSkahl <josekahl333@gmail.com>
2024-07-03 14:54:51 -0300

    limpagem do projeto e adição da listagem de produtos com a API da FakeStore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kt6vv4hpj98"
+    "revision": "0.jpu42uug4e8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
